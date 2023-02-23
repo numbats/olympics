14f2b7e628a8de858a5aa179ac0306a0c5414c8f
Jayani Lakshika <62740181+JayaniLakshika@users.noreply.github.com>
2023-02-24 00:22:32 +1100

    Upload script to create the hexsticker

---
+++ hexsticker/hexsticker.R
@@ -0,0 +1,12 @@
+#Import necessary packages
+library(hexSticker)
+library(here)
+library(showtext)
+
+# Loading Google fonts
+font_add_google("Gochi Hand", "gochi")
+#Automatically use showtext to render text for future device
+showtext_auto()
+
+sticker(here("icon_pkg.png"), package = "olympics", p_size = 25, p_y = 1.5, s_x = 1.05, s_y =0.8, s_width = .4, s_height = .2, h_fill = "#FFFFFF",p_color = "black",p_family = "gochi", filename = here("sticker.png"), h_color = "black")
+
