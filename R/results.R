11fb87d18d2fe409c1fbf854d9bbe5bb300d9780
huizezhang-sherry <huizezhangsh@gmail.com>
2023-02-23 12:45:07 +1100

    correction on paired player (synchronised) and empty results

--- R/results.R
+++ R/results.R
@@ -26,6 +26,12 @@ get_single_results <- function(url){
     rvest::html_text() %>%
     stringr::str_to_title()
 
+  if (length(name) == 0){
+    name <- html %>%
+      rvest::html_elements(".styles__AthleteName-sc-1yhe77y-3") %>%
+      rvest::html_text()
+  }
+
   country <- html %>%
     rvest::html_elements(".styles__CountryName-sc-1r5phm6-1") %>%
     rvest::html_text()
@@ -34,12 +40,24 @@ get_single_results <- function(url){
     country <- html %>%
       rvest::html_elements(".styles__CountryName-sc-rh9yz9-8") %>%
       rvest::html_text()
-
   }
 
-  res <- tibble::tibble(rank = rank, country = country, result = result)
+  res <- tibble::tibble(rank = rank, country = country)
 
+  res <- res %>% dplyr::mutate(result = c(result, rep(NA, nrow(res) - length(result))))
+
+  # correction if events are played by paired player
+  if (stringr::str_detect(url, "synchronised")){
+    name <- tibble(orig = name) %>%
+      mutate(id = rep(1:nrow(res), each = 2)) %>%
+      tidyr::nest(nested = orig) %>%
+      mutate(name = purrr::map(nested, ~dplyr::pull(.x) %>% paste0(collapse = "/ "))) %>%
+      tidyr::unnest(name) %>%
+      dplyr::pull(name)
+    }
   if (length(name) != 0){
+
+
     res <- res %>%
       dplyr::mutate(name = name) %>%
       dplyr::select(rank, country, name, result)
